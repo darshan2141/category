c59f7ebd6485aefa663856e52036a0552b053a7b
jaydeep kataria <jaydeep.kataria@eventfalcon.com>
2022-07-19 23:41:39 +0530

    category 19/07/22

---
+++ bscsci.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <nav>
+        <a href="bca.php" target="_parent">BCA</a>
+        <a href="bscit.php" target="_parent">BSC-IT</a>
+        <a href="bscsci.php">BSC-SCi</a>
+    </nav>
+
+    <?php
+    // $c = mysqli_connect("localhost", "root", "", "category");
+    include('./connection.php');
+    $category = mysqli_query($c, "select * from post where cate='bscsci' ");
+    while ($row = mysqli_fetch_array($category)) {
+    ?>
+        <div>
+            <h1><?php
+                echo $row['title'];
+                ?></h1>
+            <h2>
+                <?php
+                echo $row['content'];
+                ?>
+            </h2>
+        </div>
+    <?php
+    }
+
+    // $t = mysqli_query($c, "insert into category values(1,'BCA')");
+    ?>
+
+</body>
+
+</html>
---
+++ connection.php
@@ -0,0 +1,4 @@
+<?php
+$c = mysqli_connect("sql311.epizy.com", "epiz_31434702", "FOzfmUhIViR", "epiz_31434702_category");
+// http://sem3.ubhadiyadarshan.epizy.com/bscsci.php
+// $c = mysqli_connect("localhost", "root", "", "category");
